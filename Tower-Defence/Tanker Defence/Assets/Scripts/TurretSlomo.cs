b846db88c32c33a19ea057ce02935d54aeeebf51
QuangMinh <Gamatqua@gmail.com>
2023-12-17 19:55:09 +0700

    tower defence

---
+++ Tower-Defence/Tanker Defence/Assets/Scripts/TurretSlomo.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using static UnityEngine.GraphicsBuffer;
+
+public class TurretSlomo : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private LayerMask enemyMask;
+
+    [Header("Attribute")]
+    [SerializeField] private float targetingRange = 3f;
+    [SerializeField] private float aps = 0.5f; //attacks per second
+    [SerializeField] private float freezeTime = 1f;
+
+    private float timeUntilFire;
+
+    private void Update()
+    {
+        timeUntilFire += Time.deltaTime;
+
+        if (timeUntilFire >= (1f / aps))
+        {
+
+            FreezeEnemies();
+            timeUntilFire = 0f;
+        }
+    }
+
+    private void FreezeEnemies()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingRange, (Vector2)transform.position, 0f, enemyMask);
+
+        if (hits.Length > 0)
+        {
+            for(int i = 0; i < hits.Length; i++)
+            {
+                RaycastHit2D hit = hits[i];
+
+                EnemyMovement em = hit.transform.GetComponent<EnemyMovement>();
+                em.UpdateSpeed(0.5f);
+
+                StartCoroutine(ResetEnemySpeed(em));
+            }
+        }
+    }
+
+    private IEnumerator ResetEnemySpeed(EnemyMovement em)
+    {
+        yield return new WaitForSeconds(freezeTime);
+
+        em.resetSpeed();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Handles.color = Color.cyan;
+        Handles.DrawWireDisc(transform.position, transform.forward, targetingRange);
+    }
+}
